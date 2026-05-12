ee629a6ba5c431bb140f3701072b003864fb9013
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2026-05-12 14:59:28 +0500

    preparing for cloudflare

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,4 +1,7 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
+export const prerender = true;
+export const trailingSlash = 'always';
+
 injectAnalytics({ mode: dev ? 'development' : 'production' });
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		// adapter-static sozlamalari
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html', // SPA rejimi uchun zarur
+			precompress: false,
+			strict: true
+		}),
 		alias: {
 			'@': './src'
 		}
