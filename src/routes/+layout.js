e4be98500c7524e0a77f7e1de467354b39935677
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2026-05-12 20:31:26 +0500

    vercel leftovers removed

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,8 +1,4 @@
-import { dev } from '$app/environment';
-import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 export const prerender = false; // Hammasini prerender qilmaslik
 export const ssr = true;      // SEO uchun SSR yoqilgan
-export const trailingSlash = 'always';
-
-injectAnalytics({ mode: dev ? 'development' : 'production' });
+export const trailingSlash = 'ignore';
