74c2fac2ff1a1096fa75b68554b530a94c6830cf
lazizbekdev <lazizbektojiboyev0611@gmail.com>
2026-05-12 15:20:52 +0500

    not all of them static

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,8 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
-export const prerender = true;
+export const prerender = false; // Hammasini prerender qilmaslik
+export const ssr = false;      // SPA (Single Page App) rejimida ishlash
 export const trailingSlash = 'always';
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
